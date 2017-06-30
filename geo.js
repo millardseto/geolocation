03cd575a15331fd8af2feb88c1a6e2c8cea568c3
Millard Seto <millardseto@mac.com>
2017-06-29 23:55:43 -0700

    show output in ui

--- geo.js
+++ geo.js
@@ -6,8 +6,14 @@ function geoSuccess(position){
   console.log("geoSuccess");
   var lat = position.coords.latitude;
   var lon = position.coords.longitude;
+
+  var output = document.getElementById('output');
+  output.innerText = `lat: ${lat}  lon: ${lon}`;
 }
 function geoError(failure){
   console.log("code: " + failure.code);
   console.log("message: " + failure.message);
+
+  var output = document.getElementById('output');
+  output.innerText = `failure code: ${failure.code}  failure message: ${failure.message}`;
 }
